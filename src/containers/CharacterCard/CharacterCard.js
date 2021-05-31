55a42219e60e645f8eed9f7e9304d30c1914bec6
Wesley <wesleyadwebster@gmail.com>
2021-05-31 05:51:36 -0500

    minor CSS to make character cards align

--- src/containers/CharacterCard/CharacterCard.js
+++ src/containers/CharacterCard/CharacterCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import classes from './CharacterCard.module.css';
 
-const {cardContainer, cardInfo} = classes;
+const { cardContainer, cardInfo, cardDetails } = classes;
 
 const CharacterCard = (props) => {
+
     return (
         <div className={cardContainer}>
             <section className={cardInfo}>
                 <img src={props.chars.image} alt="character" />
-                <span><label>Name:</label> {props.chars.name}</span>
-                <span><label>Species:</label> {props.chars.species}</span>
-                <span><label>Status:</label> {props.chars.status}</span>
+                <div className={cardDetails}>
+                    <span><label>Name:</label> {props.chars.name}</span>
+                    <span><label>Species:</label> {props.chars.species}</span>
+                    <span><label>Status:</label> {props.chars.status}</span>
+                </div>
             </section>
         </div>
     );
